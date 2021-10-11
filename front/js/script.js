4fd48c70819b80c35903413edf33ab60bec6bbf8
Florent Magnoac <flomag5@hotmail.fr>
2021-10-11 12:26:51 +0200

    index.html_insertion-items

--- front/js/script.js
+++ front/js/script.js
@@ -6,19 +6,48 @@ fetch("http://localhost:3000/api/products")
         return res.json();
     }
 })
-.then(function(items) {
-    myLog(items)
-})
+
 .catch(function(err) {
     console.log("Fetch Failed")
-   alert("Affichage momentanément impossible. Désolé pour le dérangement")
+    let items = document.querySelector("#items");
+    items.innertHTML = "Affichage momentanément impossible. Veuillez revenir plus tard.";
+})
+
+.then(function(allItems) {
+    const items = allItems;
+    console.log(items);
+    for (let article in items) {
+
+        let productLink = document.createElement("a");
+        document.querySelector(".items").appendChild(productLink);
+        productLink.href = `product.html?id=${allItems[article]._id}`;
+
+        let itemArticle = document.createElement("article");
+        productLink.appendChild(itemArticle);
+
+        let productImg = document.createElement("img");
+        itemArticle.appendChild(productImg);
+        productImg.src = allItems[article].imageUrl;
+        productImg.alt = allItems[article].altTxt;
+
+        let productName = document.createElement("h3");
+        itemArticle.appendChild(productName);
+        productName.classList.add("productName");
+        productName.innerHTML = allItems[article].name;
+
+        let productDescription = document.createElement("p");
+        itemArticle.appendChild(productDescription);
+        productDescription.classList.add("productDescription");
+        productDescription.innerHTML = allItems[article].description;
+    }
 });
 
 
-function myLog(myItems){
-    console.log(myItems)
-}
+
+let itemsData = document.getElementById("items");
 
 
-let data = document.getElementById("items");
-data.innertHTML = ""
+
+
+
+

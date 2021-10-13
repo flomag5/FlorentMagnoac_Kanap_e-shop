8a2d412d7f9d80b1e139d93b502a4866307840be
Florent Magnoac <flomag5@hotmail.fr>
2021-10-13 15:56:23 +0200

    script-product.js_better-commentaries

--- front/js/product.js
+++ front/js/product.js
@@ -9,12 +9,12 @@ console.log(urlSearchParams);
 const idItems = urlSearchParams.get("id");
 console.log(idItems);
 
-//Positionnement de l'objet
+//Positionnement de l'objet dans le DOM
 const positionElement = document.querySelector(".item");
 console.log(positionElement);
 
 
-// Récupération d'un produit depuis son Id
+// Récupération d'un article de l'API avec son Id
 
 fetch("http://localhost:3000/api/products/" + idItems)
     .then(function (res) {
@@ -24,26 +24,29 @@ fetch("http://localhost:3000/api/products/" + idItems)
         console.log("Fetch failed");
     })
 
+    // Fonction de récupération des informations de l'article
     .then(function (itemData) {
         article = itemData;
         
+    // Affichage de l'image de l'article choisi
      let productImg = document.createElement("img");
      document.querySelector(".item__img").appendChild(productImg);
      productImg.src = article.imageUrl;
      productImg.alt = article.altTxt;
 
-     
+     // Nom de l'article choisi
      let productName = document.getElementById('title');
      productName.innerHTML = article.name;
 
-     
+     // Affiche le prix de l'article choisi
      let productPrice = document.getElementById('price');
-     productPrice.innerHTML = article.price /100;
+     productPrice.innerHTML = article.price /100;  // prix affiché en euros
 
-     
+     // Affiche la description de l'article choisi
      let productDescription = document.getElementById('description');
      productDescription.innerHTML = article.description;
 
+     // Sélection du choix de la couleur de l'article
 for (let colors of article.colors){
     console.log(colors);
      let productColors = document.createElement("option");
--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,4 @@
-// Récupération des produits depuis l'API
+// Requête de récupération des produits depuis l'API
 
 fetch("http://localhost:3000/api/products")
     .then(function(res) {
@@ -10,17 +10,25 @@ fetch("http://localhost:3000/api/products")
     .catch(function(err) {
         console.log("Fetch Failed")
         let items = document.querySelector("#items");
-        items.innertHTML = "Affichage momentanément impossible. Veuillez revenir plus tard.";
+        items.innertHTML = "Affichage momentanément indisponible. Veuillez revenir plus tard.";
 })
 
-// Fonction qui va afficher les objets dans le DOM automatiquement
-.then(function(allItems) {
-    const items = allItems;
-    console.log(items);
+
+/* Récupération de la réponse émise
+---Fonction qui va afficher les objets dans le DOM automatiquement --- */
+
+    .then(function(allItems) {
+        const items = allItems;
+        console.log(items);
 
 //Boucle pour afficher chaque article de tous les produits
     for (let article in items) {
-// Créer les éléments et mettre les données à l'intérieur
+
+/* Insertion de chaque produit
+--- Créer les éléments html et mettre les données à l'intérieur --- */
+
+        /* Paramétrage de l'attribut 'href' de la balise 'a' 
+        --- avec ID pour récupération de l'article depuis la page produit --- */
         let productLink = document.createElement("a");
         document.querySelector(".items").appendChild(productLink);
         productLink.href = `product.html?id=${allItems[article]._id}`;
@@ -28,16 +36,19 @@ fetch("http://localhost:3000/api/products")
         let itemArticle = document.createElement("article");
         productLink.appendChild(itemArticle);
 
+        // Affiche image et texte alternatif
         let productImg = document.createElement("img");
         itemArticle.appendChild(productImg);
         productImg.src = allItems[article].imageUrl;
         productImg.alt = allItems[article].altTxt;
 
+        // Affichage du nom du produit
         let productName = document.createElement("h3");
         itemArticle.appendChild(productName);
         productName.classList.add("productName");
         productName.innerHTML = allItems[article].name;
 
+        // Affichage de la description du produit
         let productDescription = document.createElement("p");
         itemArticle.appendChild(productDescription);
         productDescription.classList.add("productDescription");

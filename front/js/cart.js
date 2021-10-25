2187fdcf13309f0d74d0090a246b6712dc71d281
Florent Magnoac <flomag5@hotmail.fr>
2021-10-25 18:19:43 +0200

    cart.js-form-validation_input

--- front/js/cart.js
+++ front/js/cart.js
@@ -28,6 +28,7 @@ if (cartItem === null || cartItem == 0) {
 
 // Fonction affichage des informations de chaque produit dans le panier
 function displayItem(data, color, quantity) {
+  let price = data.price * quantity;
   return `
          <article class="cart__item" data-id="${data._Id}">
                  <div class="cart__item__img">
@@ -37,7 +38,7 @@ function displayItem(data, color, quantity) {
                    <div class="cart__item__content__titlePrice">
                      <h2>${data.name}</h2>
                      <p>${color}</p>
-                     <p>${data.price * quantity}.00 €</p>
+                     <p>${price}.00 €</p>
                    </div>
                    <div class="cart__item__content__settings">
                      <div class="cart__item__content__settings__quantity">
@@ -50,10 +51,34 @@ function displayItem(data, color, quantity) {
                    </div>
                  </div>
                </article>`;
-
 }
 
+//Fonction permettant de crée un total de la commande
+let totalPriceCart = 0;
+cartItem.forEach((items) => {
+  totalPriceCart.valueAsNumber = totalPriceCart + items.price * items.quantity;
+});
+const totalPrice = document.getElementById("totalPrice");
+totalPrice.innerHTML = totalPriceCart;
+console.log(totalPriceCart);
+
+
+
+let arrayQuantities = [];
+for (let items of cartItem) {
+  let ItemQuantity = items.quantity;
+
+  arrayQuantities.push(ItemQuantity);
+}
+console.log(arrayQuantities);
+
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+let totalQuantityCart = arrayQuantities.reduce(reducer);
+document.querySelector("#totalQuantity").innerHTML = totalQuantityCart;
+
+
 //------- Fin affichage produits panier -------------
+
 
 // Modification d'une quantité de produit
 function modifyQtt() {
@@ -81,38 +106,35 @@ function modifyQtt() {
 }
 modifyQtt();
 
+
+
 /*
-function getPrices() {
- 
-}
-getPrices();
- 
  
 //------- Suppression d'un article du panier --------------
  
 function deleteArticle() {
-  // Sélection du bouton supprimer l'article
-  const deleteItem = document.getElementsByClassName("deleteItem");
-  console.log(deleteItem);
+ // Sélection du bouton supprimer l'article
+ const deleteItem = document.getElementsByClassName("deleteItem");
+ console.log(deleteItem);
  
-  // Fonction de suppression d'un article du panier
-  for (let l = 0; l < deleteItem.length; l++) {
-    deleteItem[l].addEventListener("click", (event) => {
-      event.preventDefault();
-      // Sélection de l'article à supprimer
-      let selectId = cartItem[l].element.dataset._id;
-      let selectColor = cartItem[l].element.dataset.color;
-      console.log(selectColor);
-      // Méthode filter pour sélectionner les éléments à garder et supprimer l'élément sélectionné
-      cartItem = cartItem.filter(element => element._id !== selectId);
+ // Fonction de suppression d'un article du panier
+ for (let l = 0; l < deleteItem.length; l++) {
+   deleteItem[l].addEventListener("click", (event) => {
+     event.preventDefault();
+     // Sélection de l'article à supprimer
+     let selectId = cartItem[l].element.dataset._id;
+     let selectColor = cartItem[l].element.dataset.color;
+     console.log(selectColor);
+     // Méthode filter pour sélectionner les éléments à garder et supprimer l'élément sélectionné
+     cartItem = cartItem.filter(element => element._id !== selectId);
  
-      localStorage.setItem("cart", JSON.stringify(cartItem));
+     localStorage.setItem("cart", JSON.stringify(cartItem));
  
-      // Alerte de suppression du produit
-      alert("Ce produit a été supprimé de votre panier");
-      window.location.href = "cart.html";
-    })
-  }
+     // Alerte de suppression du produit
+     alert("Ce produit a été supprimé de votre panier");
+     window.location.href = "cart.html";
+   })
+ }
 }
 deleteArticle();*/
 
@@ -135,14 +157,88 @@ orderButton.addEventListener("click", (e) => {
   }
   console.log("formValues", formValues);
 
-  //-------------- Gestion de validation du formulaire
-  const leprenom = formValues.prenom;
-  console.log(leprenom);
+
+  //-------------- Gestion de validation du formulaire ----------
+
+  // Fonction de contrôle des saisies selon regEx
+  const regExNameCity = (value) => {
+    return /^[A-Za-z ,.'-]{3,20}$/.test(value)
+  }
+
+  const regExAddress = (value) => {
+    return /^[0-9]{1,3}(?:(?:[,. ]){1}[-a-zA-Zàâäéèêëïîôöùûüç]+)+/.test(value)
+  }
+
+  const regExEmail = (value) => {
+    return /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(value)
+  }
+
+  function firstNameCheck() {
+    const lePrenom = formValues.prenom;
+    if (regExNameCity(lePrenom)) {
+      firstNameErrorMsg.innerHTML = " ";
+      return true;
+    } else {
+      firstNameErrorMsg.innerHTML = "Veuillez renseigner ce champs";
+      return false;
+    };
+  }
+
+  function lastNameCheck() {
+    const leNom = formValues.nom;
+    if (regExNameCity(leNom)) {
+      lastNameErrorMsg.innerHTML = " ";
+      return true;
+    } else {
+      lastNameErrorMsg.innerHTML = "Veuillez renseigner ce champs";
+      return false;
+    };
+  }
+
+  function addressCheck() {
+    const leAddress = formValues.adresse;
+    if (regExAddress(leAddress)) {
+      addressErrorMsg.innerHTML = " ";
+      return true;
+    } else {
+      addressErrorMsg.innerHTML = "Veuillez saisir une adresse valide";
+      return false;
+    };
+  }
+
+  function cityCheck() {
+    const laVille = formValues.ville;
+    if (regExNameCity(laVille)) {
+      cityErrorMsg.innerHTML = " ";
+      return true;
+    } else {
+      cityErrorMsg.innerHTML = "Veuillez renseigner une ville";
+      return false;
+    };
+  }
+
+  function eMailCheck() {
+    const leMail = formValues.email;
+    if (regExEmail(leMail)) {
+      console.log("ok");
+      emailErrorMsg.innerHTML = " ";
+      return true;
+
+    } else {
+      console.log("ko");
+      emailErrorMsg.innerHTML = "Adresse email n'est pas valide";
+      return false;
+    };
+  }
 
 
-
-  // Mettre l'objet dans le local storage
-  localStorage.setItem("formValues", JSON.stringify(formValues));
+  // Contrôle validité du formulaire avant envoi dans local storage
+  if (firstNameCheck() && lastNameCheck() && addressCheck() && cityCheck() && eMailCheck()) {
+    // Mettre l'objet dans le local storage
+    localStorage.setItem("formValues", JSON.stringify(formValues));
+  } else {
+    console.log("Veuillez remplir les champs du formulaire");
+  }
 
   // Mettre les valeurs du formulaire et les produits du panier dans un objet à envoyer vers le serveur
   const toSend = {

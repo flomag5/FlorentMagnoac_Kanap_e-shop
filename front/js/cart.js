7ffc9e0a092b7339677490f162beb9b2c7c56e86
Florent Magnoac <flomag5@hotmail.fr>
2021-10-27 18:30:52 +0200

    cart.js_test-POST

--- front/js/cart.js
+++ front/js/cart.js
@@ -29,8 +29,15 @@ if (cartItem === null || cartItem == 0) {
 // Fonction affichage des informations de chaque produit dans le panier
 function displayItem(data, color, quantity) {
   let price = data.price * quantity;
+
+  // Calcul prix total du panier
+  let prices = [];
+  prices.push(price);
+  let totalPrice = prices.reduce((acc, el) => acc + el);
+  document.querySelector("#totalPrice").innerHTML += totalPrice;
+
   return `
-         <article class="cart__item" data-id="${data._Id}">
+         <article class="cart__item" data-id="${data._Id}" data-color="${color}">
                  <div class="cart__item__img">
                    <img src=${data.imageUrl} alt="${data.altTxt}">
                  </div>
@@ -38,7 +45,7 @@ function displayItem(data, color, quantity) {
                    <div class="cart__item__content__titlePrice">
                      <h2>${data.name}</h2>
                      <p>${color}</p>
-                     <p>${price}.00 €</p>
+                     <p id="price">${price}.00 €</p>
                    </div>
                    <div class="cart__item__content__settings">
                      <div class="cart__item__content__settings__quantity">
@@ -51,17 +58,22 @@ function displayItem(data, color, quantity) {
                    </div>
                  </div>
                </article>`;
+
 }
 
-//Fonction permettant de crée un total de la commande
-let totalPriceCart = 0;
-cartItem.forEach((items) => {
-  totalPriceCart.valueAsNumber = totalPriceCart + items.price * items.quantity;
-});
-const totalPrice = document.getElementById("totalPrice");
-totalPrice.innerHTML = totalPriceCart;
-console.log(totalPriceCart);
 
+// Déclaration de la variable de calcul du nombre d'articles dans le panier
+/*let arrayPrices = [];
+for (let items of cartItem) {
+  let ItemPrice = items.price;
+  console.log(items.price)
+  arrayPrices.push(ItemPrice);
+}
+console.log(arrayPrices);
+
+const reduces = (previousValue, currentValue) => previousValue + currentValue;
+let totalPriceCart = arrayPrices.reduce(reduces);
+document.querySelector("#totalPrice").innerHTML = totalPriceCart;*/
 
 
 let arrayQuantities = [];
@@ -81,84 +93,65 @@ document.querySelector("#totalQuantity").innerHTML = totalQuantityCart;
 
 
 // Modification d'une quantité de produit
-function modifyQtt() {
-  let qttModif = document.querySelectorAll(".itemQuantity");
 
-  for (let k = 0; k < qttModif.length; k++) {
-    qttModif[k].addEventListener("change", (event) => {
+
+
+
+//------- Suppression d'un article du panier --------------
+
+function deleteItem() {
+  // Sélection du bouton supprimer l'article
+  const deleteItem = document.querySelectorAll(".deleteItem");
+  console.log(deleteItem);
+
+  // Fonction de suppression d'un article du panier
+  for (let l = 0; l < deleteItem.length; l++) {
+    deleteItem[l].addEventListener("click", (event) => {
       event.preventDefault();
-
-      //Selection de l'element à modifier en fonction de son id ET sa couleur
-      let quantityModif = cartItem[k].items.quantity;
-      let qttModifValue = qttModif[k].valueAsNumber;
-
-      const resultFind = cartItem.find((el) => el.qttModifValue !== quantityModif);
-
-      resultFind.items.quantity = qttModifValue;
-      cartItem[k].items.quantity = resultFind.items.quantity;
+      // Sélection de l'article à supprimer
+      let selectId = this.dataset.id;
+      let selectColor = this.dataset.color;
+      console.log(selectColor);
+      console.log(selectId);
+      // Méthode filter pour sélectionner les éléments à garder et supprimer l'élément sélectionné
+      cartItem = cartItem.filter(element => element.id !== selectId || element.dataset.color !== selectColor);
 
       localStorage.setItem("cart", JSON.stringify(cartItem));
 
-      // refresh rapide
+      // Alerte de suppression du produit
+      alert("Ce produit a été supprimé de votre panier");
       location.reload();
     });
-  };
+  }
 }
-modifyQtt();
 
 
-
-/*
- 
-//------- Suppression d'un article du panier --------------
- 
-function deleteArticle() {
- // Sélection du bouton supprimer l'article
- const deleteItem = document.getElementsByClassName("deleteItem");
- console.log(deleteItem);
- 
- // Fonction de suppression d'un article du panier
- for (let l = 0; l < deleteItem.length; l++) {
-   deleteItem[l].addEventListener("click", (event) => {
-     event.preventDefault();
-     // Sélection de l'article à supprimer
-     let selectId = cartItem[l].element.dataset._id;
-     let selectColor = cartItem[l].element.dataset.color;
-     console.log(selectColor);
-     // Méthode filter pour sélectionner les éléments à garder et supprimer l'élément sélectionné
-     cartItem = cartItem.filter(element => element._id !== selectId);
- 
-     localStorage.setItem("cart", JSON.stringify(cartItem));
- 
-     // Alerte de suppression du produit
-     alert("Ce produit a été supprimé de votre panier");
-     window.location.href = "cart.html";
-   })
- }
-}
-deleteArticle();*/
-
-//------------- Formulaire de commande
+//------------- Formulaire de commande -------------------------------
 
 // Sélection bouton envoi du formulaire
 const orderButton = document.querySelector("#order");
-console.log(orderButton);
 
 // Add event listener
 orderButton.addEventListener("click", (e) => {
   e.preventDefault();
 
-  const formValues = {
-    prenom: document.querySelector("#firstName").value,
-    nom: document.querySelector("#lastName").value,
-    adresse: document.querySelector("#address").value,
-    ville: document.querySelector("#city").value,
-    email: document.querySelector("#email").value,
+
+  // Création d'une classe pour fabrique l'objet dans lequel iront les valeurs du formulaire
+  class Formulaire {
+    constructor() {
+      this.prenom = document.querySelector("#firstName").value;
+      this.nom = document.querySelector("#lastName").value;
+      this.adresse = document.querySelector("#address").value;
+      this.ville = document.querySelector("#city").value;
+      this.email = document.querySelector("#email").value;
+      //this.input = document.querySelector(`#${input}`).value;
+    }
   }
-  console.log("formValues", formValues);
+  // Appel de l'instance de classe formulaire pour créer l'objet formValues
+  const formValues = new Formulaire();
 
 
-  //-------------- Gestion de validation du formulaire ----------
+  //-------------- Gestion de validation du formulaire --------
 
   // Fonction de contrôle des saisies selon regEx
   const regExNameCity = (value) => {
@@ -179,7 +172,7 @@ orderButton.addEventListener("click", (e) => {
       firstNameErrorMsg.innerHTML = " ";
       return true;
     } else {
-      firstNameErrorMsg.innerHTML = "Veuillez renseigner ce champs";
+      firstNameErrorMsg.innerHTML = "Veuillez renseigner ce champ";
       return false;
     };
   }
@@ -190,7 +183,7 @@ orderButton.addEventListener("click", (e) => {
       lastNameErrorMsg.innerHTML = " ";
       return true;
     } else {
-      lastNameErrorMsg.innerHTML = "Veuillez renseigner ce champs";
+      lastNameErrorMsg.innerHTML = "Veuillez renseigner ce champ";
       return false;
     };
   }
@@ -241,18 +234,68 @@ orderButton.addEventListener("click", (e) => {
   }
 
   // Mettre les valeurs du formulaire et les produits du panier dans un objet à envoyer vers le serveur
-  const toSend = {
+  const order = {
     cartItem,
     formValues
   }
-  console.log("Envoyer", toSend);
+  console.log("Envoyer", order);
+
+  /**Expect request to contain:
+   * contact: {
+   * firstName:
+   * lastName:
+   * address:
+   * city:
+   * email:
+   * }
+   * products: [strings] -> products id
+   */
+  const promise = fetch("http://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    body: JSON.stringify(order),
+    headers: {
+      'Accept': "application/json",
+      "Content-type": "application/json",
+    },
+  });
+  console.log("PRM", promise);
+
+  // Pour visualiser résultat du serveur dans la console
+  promise.then(async (response) => {
+    try {
+      console.log("RESP", response);
+      const contenu = await response.json();
+      console.log("CONTENT", contenu);
+    } catch (e) {
+      console.log(e);
+    }
+  })
+
+  // Pour voir ce qu'il y a réellement sur le serveur
+  /*const newPromise = fetch("http://localhost:3000/api/products/order")
+  newPromise.then(async (response) => {
+    try {
+      console.log("RESULT", newPromise);
+      const dataBack = await response.json()
+      console.log("DATA", dataBack);
+    } catch (e) {
+      console.log(e);
+    }
+  })*/
 });
+
 
 // Récupération des valeurs du formulaire depuis local storage
 const dataUser = localStorage.getItem("formValues");
 const dataUserObject = JSON.parse(dataUser);
 
-
+function fillInputData(input) {
+  if (dataUserObject == null) {
+    console.log("local storage a pour valeur null");
+  } else {
+    document.querySelector(`#${input}`).value = dataUserObject[input];
+  }
+}
 // Mettre les valeurs du local storage dans le champ du formulaire
 document.querySelector("#firstName").value = dataUserObject.prenom;
 document.querySelector("#lastName").value = dataUserObject.nom;
@@ -260,4 +303,4 @@ document.querySelector("#address").value = dataUserObject.adresse;
 document.querySelector("#city").value = dataUserObject.ville;
 document.querySelector("#email").value = dataUserObject.email;
 
-console.log("dataUserObject", dataUserObject);
+
